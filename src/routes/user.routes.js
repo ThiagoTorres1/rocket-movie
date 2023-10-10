4947e974f7f3e8e56e4ab1b716c53ce4b14b020f
Thiago Torres Costa <thiago.t0rres1@hotmail.com>
2023-10-10 09:37:03 -0300

    saving image in DB

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,20 @@
 const {Router} = require("express")
+const multer = require("multer")
+const uploadConfig = require("../config/upload")
+
 const UserController = require("../controller/userController")
+const UserAvatarController = require("../controller/userAvatarController")
+
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
+
 const usersRoutes = Router()
+const upload = multer(uploadConfig.MULTER)
 
 const userController = new UserController()
+const userAvatarController = new UserAvatarController()
 
 usersRoutes.post("/", userController.create)
 usersRoutes.put("/", ensureAuthenticated, userController.update)
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update)
 
 module.exports = usersRoutes
