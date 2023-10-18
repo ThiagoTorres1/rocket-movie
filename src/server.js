0576013dfa22c7a391945abd26acc2e872c25a59
Thiago Torres Costa <thiago.t0rres1@hotmail.com>
2023-10-18 07:38:31 -0300

    adding dotenv

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require("express-async-errors")
+require("dotenv/config")
 
 const database = require("./database/sqlite")
 const AppError = require("./utils/AppError")
@@ -32,6 +33,6 @@ app.use((error, request, response, next) => {
   })
 })
 
-const PORT = 3333
+const PORT = process.env.PORT || 3333
 
 app.listen(PORT, () => console.log(`Servidor funcionando na porta ${PORT}`))
